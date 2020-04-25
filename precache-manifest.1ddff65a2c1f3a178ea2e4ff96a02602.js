2c4d28432faff207684021d0a85b25b17d4dfd54
Kittisak Ardkonghan <kittisak@star-platinum.local>
2020-04-25 18:56:12 +0700

    Updates

---
+++ precache-manifest.1ddff65a2c1f3a178ea2e4ff96a02602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd129dca3a7765632a53a384b8cd6ad",
+    "url": "/detect-face/index.html"
+  },
+  {
+    "revision": "be1edd7bc39ada55d231",
+    "url": "/detect-face/static/css/main.507cca9e.chunk.css"
+  },
+  {
+    "revision": "462531c3031c301ae02b",
+    "url": "/detect-face/static/js/2.1f011004.chunk.js"
+  },
+  {
+    "revision": "be1edd7bc39ada55d231",
+    "url": "/detect-face/static/js/main.34eedb83.chunk.js"
+  },
+  {
+    "revision": "ba403e212d6c881bfb03",
+    "url": "/detect-face/static/js/runtime~main.6ca8ce3a.js"
+  }
+]);
